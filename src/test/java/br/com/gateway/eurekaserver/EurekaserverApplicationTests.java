154da3140c22dbb0bdbb2cd19caeabffb61b3440
Marcus <flambres3@gmail.com>
2023-10-29 19:56:25 -0300

    Initial commit

---
+++ src/test/java/br/com/gateway/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gateway.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
